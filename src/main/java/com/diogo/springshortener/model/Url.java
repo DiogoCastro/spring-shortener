2706043eae6c132b5888e69595eb80a12efff2fa
Diogo Castro <diogooncastro1@gmail.com>
2019-10-22 10:57:30 -0300

    Finishing the api (Just need to handle some errors output)

--- src/main/java/com/diogo/springshortener/model/Url.java
+++ src/main/java/com/diogo/springshortener/model/Url.java
@@ -1,14 +1,18 @@
 package com.diogo.springshortener.model;
 
 import java.io.Serializable;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.URL;
 
 import com.diogo.springshortener.commom.Converter;
 
@@ -23,14 +27,14 @@ public class Url implements Serializable{
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private long id;
 	
-	@NotNull
+	@NotEmpty
+	@URL
 	private String url;
 	
 	private String urlShortened;
 	
 	@NotNull
 	private Boolean active;
-	
 	
 	public long getId() {
 		return id;
@@ -45,21 +49,22 @@ public class Url implements Serializable{
 	}
 	
 	public void setUrl(String url) {
-		if (!url.contains("://")) {
+		if (!url.contains("://") && !url.isEmpty()) {
 			this.url = "http://" + url;
 		} else
 			this.url = url;
 	}
 	
-	public String getUrlShortened() {
-		return urlShortened;
+	public String getUrlShortened() throws UnknownHostException {
+		String host = InetAddress.getLoopbackAddress().getHostAddress();
+		return host + ":8080/api/url/" + urlShortened;
 	}
 	
 	public void setUrlShortened(String urlShortened) {
 		Converter converter = new Converter();
 		if (urlShortened.isEmpty()) {
 			this.urlShortened = converter.getGenerateName(6);
-		} else 
+		} else
 			this.urlShortened = urlShortened;
 	}
 	
