907a4f034fed9bcb61221ccf569fec532dd12632
Diogo Castro <diogooncastro1@gmail.com>
2019-10-22 11:58:46 -0300

    Enabling CORS to Heroku.

--- src/main/java/com/diogo/springshortener/resources/UrlController.java
+++ src/main/java/com/diogo/springshortener/resources/UrlController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,6 +23,7 @@ import org.springframework.web.servlet.view.RedirectView;
 import com.diogo.springshortener.model.Url;
 import com.diogo.springshortener.service.UrlService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api")
 public class UrlController {
